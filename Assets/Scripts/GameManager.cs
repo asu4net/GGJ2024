860d0f68ac453be5883d032ab70f5c99288c6c3b
jmguillenfernandez <61391449+jmguillenfernandez@users.noreply.github.com>
2024-01-27 15:09:17 +0100

    TMP + Main Menu + Credits + BGSFX + Fart

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject MenuObject;
+    public GameObject CreditsObject;
+    private void Awake()
+    {
+        Screen.SetResolution(1920, 1080, true);
+    }
+
+    public void BeginGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void GoToCredits()
+    {
+        MenuObject.SetActive(false);
+        CreditsObject.SetActive(true);
+    }
+    public void GoToMenu()
+    {
+        MenuObject.SetActive(true);
+        CreditsObject.SetActive(false);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
