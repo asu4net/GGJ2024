c40cb697264c2a11dc1b5b7e3554f41a3741149a
Jagarhe <jvrgrchrmsll@gmail.com>
2024-01-27 23:47:56 +0100

    UI Navegable por los mares

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject MenuObject;
     public GameObject CreditsObject;
-   
+
+    public GameObject Credits_first_selected;
+    public GameObject Principal_first_selected;
+
     private void Awake()
     {
         Screen.SetResolution(1920, 1080, true);
@@ -21,11 +25,15 @@ public class GameManager : MonoBehaviour
     {
         MenuObject.SetActive(false);
         CreditsObject.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(Credits_first_selected);
     }
     public void GoToMenu()
     {
         MenuObject.SetActive(true);
         CreditsObject.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(Principal_first_selected);
     }
 
     public void CloseGame()
